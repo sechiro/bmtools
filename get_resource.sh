8effb0c2a059a927ddb8050f1a94b71af660459f
Seiichiro, Ishida <ishida.seiichiro@gmail.com>
2013-04-24 12:13:37 +0900

    Get kSar script

---
+++ get_resource.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Get kSar5.0.6
+/usr/bin/wget -P resources/ http://downloads.sourceforge.net/project/ksar/ksar/5.0.6/kSar-5.0.6.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fksar%2Ffiles%2Flatest%2Fdownload&ts=1368281000&use_mirror=jaist
+/usr/bin/unzip -o resources/kSar-5.0.6.zip -d resources/
